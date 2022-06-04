2040363ea202a42f5f6cfecbe5dee9d46f1b6354
@CesarMorales01 <cezar_mh86@hotmail.com>
2022-06-03 22:07:38 -0500

    :tada: primera fase del front end

---
+++ src/components/estado/Estado.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect } from 'react'
+import { obtenerTodos } from '../../services/EstadoService';
+
+export default function Estado() {
+
+  useEffect( () => {
+    const getEstados = () => {
+        obtenerTodos().then(r => {
+            console.log(r.data)
+        }).catch(e => {
+            console.log(e)
+        })
+    };
+    getEstados();
+  }, []);
+
+
+  return (
+    <div className='container'>Estado</div>
+  )
+}
